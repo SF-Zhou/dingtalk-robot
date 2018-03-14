c9194eb0e9032779b26d205c742c0b0b5ce149db
SF-Zhou <sfzhou.scut@gmail.com>
2018-03-14 23:38:20 +0800

    step 1: implement core function

---
+++ dingtalk_robot.py
@@ -0,0 +1,31 @@
+import requests
+__version__ = '0.0.1'
+
+
+class DingtalkRobot:
+    BaseUrl = 'https://oapi.dingtalk.com/robot/send?access_token='
+
+    def __init__(self, token: str):
+        self.token = token
+        self.access_url = self.BaseUrl + token
+
+    def send_text(self, content: str) -> bool:
+        message = {
+            'msgtype': 'text',
+            'text': {
+                'content': content
+            }
+        }
+
+        response = requests.post(url=self.access_url, json=message)
+        status = response.json()
+        if status['errcode'] != 0:
+            raise DingtalkRobot.Error('Error Code: {}, {}'.format(
+                status['errcode'],
+                status['errmsg']
+            ))
+
+        return True
+
+    class Error(ValueError):
+        pass
